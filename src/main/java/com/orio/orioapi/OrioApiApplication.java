860996a7a1f584c24c5c447bbb176abb73b2dfd0
BenoitNE <benoitnerin@gmail.com>
2024-02-22 23:19:21 +0100

    Premier commit

---
+++ src/main/java/com/orio/orioapi/OrioApiApplication.java
@@ -0,0 +1,13 @@
+package com.orio.orioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orio/orioapi/OrioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orio.orioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
