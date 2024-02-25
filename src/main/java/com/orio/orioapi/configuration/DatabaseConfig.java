d8e14738614ed651f3d31c170fa54badd09f730b
BenoitNE <benoitnerin@gmail.com>
2024-02-25 21:31:44 +0100

    Ajout d'un base de donnée de démo

---
+++ src/main/java/com/orio/orioapi/configuration/DatabaseConfig.java
@@ -0,0 +1,24 @@
+package com.orio.orioapi.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
+        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
+        resourceDatabasePopulator.addScript(new ClassPathResource("/demo_db.sql"));
+
+        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
+        dataSourceInitializer.setDataSource(dataSource);
+        dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
+        return dataSourceInitializer;
+    }
+}
--- src/main/java/com/orio/orioapi/persistence/entity/JobDescription.java
+++ src/main/java/com/orio/orioapi/persistence/entity/JobDescription.java
@@ -12,7 +12,7 @@ import java.util.List;
 public class JobDescription {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String job;
